d9f9baae49e544831ecb6b2bd44549ad1cba6579
roman-cloudStudent <roman.demin.info@gmail.com>
2022-10-11 15:26:13 +0300

    добавил решение задач

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,3 +4,13 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine($"третья цифра => " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
